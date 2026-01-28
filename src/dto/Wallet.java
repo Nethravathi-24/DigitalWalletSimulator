26c079bf5790b0191076a01a345d2bb231d683e8
Nethravathi-24 <nethravathid490@gmail.com>
2026-01-28 21:45:06 +0530

    Add files via upload

---
+++ src/dto/Wallet.java
@@ -0,0 +1,46 @@
+package dto;
+
+/*
+ * DTO (Data Transfer Object)
+ * Used to carry data between layers
+ */
+public class Wallet {
+
+    private int id;
+    private String name;
+    private String phone;
+    private double balance;
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
